c4894b33fb94112be50e6ede2a078b18dbe0b0c6
Benjamin Stigsen <benstigsen@posteo.net>
2023-04-30 02:09:29 +0200

    added comments

--- minicss.go
+++ minicss.go
@@ -8,11 +8,13 @@ func Minify(data []byte) []byte {
 
 		start := i
 		if i < len(data)-1 && a == '/' {
+			// Single-line comment
 			if data[i+1] == '/' {
 				for i < len(data) && data[i] != '\n' {
 					i += 1
 				}
 				continue
+			// Block comment
 			} else if data[i+1] == '*' {
 				for i < len(data)-1 {
 					i += 1
@@ -30,7 +32,6 @@ func Minify(data []byte) []byte {
 
 				continue
 			}
-			// Block Comment
 		}
 
 		// https://pkg.go.dev/unicode#IsSpace
@@ -38,6 +39,7 @@ func Minify(data []byte) []byte {
 			continue
 		}
 
+		// String
 		if a == '\'' || a == '"' {
 			for _, b := range data[i+1:] {
 				i += 1
